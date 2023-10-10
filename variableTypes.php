3fb4e5d98b8e90a21df165dd16c9ced0f7b83e07
Renz <renzlouisd@gmail.com>
2023-10-11 03:15:11 +0800

    Variable Types

---
+++ variableTypes.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // Scalar types (contains one value)
+        $string = "Renz";
+        $int = 9;
+        $float = 9.5;
+        $bool = true;
+
+        // Array type
+        $names = ["Renz", "Louis", "Desiderio"];
+
+        echo $string;
+    ?>
+
+</body>
+</html>
